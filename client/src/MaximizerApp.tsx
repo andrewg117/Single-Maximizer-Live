172f61b78c0afe3a0ab55bce235df0ef2f599ad3
andrewg117 <andrewjgrant7@gmail.com>
2024-09-15 12:45:31 -0400

    Added code for createRoutesFromElements

--- client/src/MaximizerApp.tsx
+++ client/src/MaximizerApp.tsx
@@ -1,39 +1,20 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
+import { Outlet, Route } from "react-router-dom";
 import { useAppSelector } from "./app/hooks";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Home from "./pages/Home";
-import AboutUs from "./pages/AboutUs";
-import FAQ from "./pages/FAQ";
-import EmailSignUp from "./pages/EmailSignUp";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
-import NavBar from "./components/NavBar";
 import Profile from "./pages/Profile";
-import ProfileEdit from "./pages/ProfileEdit";
-import Singles from "./pages/Singles";
-import SingleEdit from "./pages/SingleEdit";
-import SingleView from "./pages/SingleView";
-import NewRelease from "./pages/NewRelease";
-import CheckoutPage from "./pages/CheckoutPage";
+import NavBar from "./components/NavBar";
 import NavBarLeft from "./components/NavBarLeft";
-import TokenCheck from "./components/TokenCheck";
 import styles from "./css/style.module.css";
 
 function MaximizerApp() {
   const { user } = useAppSelector((state) => state.auth);
   return (
     <>
-      {/* <Router> */}
-        <section
-          id={user ? styles.profile_body_wrapper : styles.body_wrapper}
-        >
-          {user ? <NavBarLeft /> : <NavBar />}
-          <Outlet />
-          <Routes>
+      <section id={user ? styles.profile_body_wrapper : styles.body_wrapper}>
+        {user ? <NavBarLeft /> : <NavBar />}
+        
             {/* {user === null ? (
               <Route
                 path="*"
@@ -45,72 +26,8 @@ function MaximizerApp() {
                 element={<Profile />}
               />
             )} */}
-            
-            {/* <Route
-              path="/home"
-              element={<Home />}
-            />
-            <Route
-              path="/home/aboutus"
-              element={<AboutUs />}
-            />
-            <Route
-              path="/home/faq"
-              element={<FAQ />}
-            />
-            <Route
-              path="/home/signup/:token"
-              element={<SignUp />}
-            />
-            <Route
-              path="/home/emailsignup"
-              element={<EmailSignUp />}
-            />
-            <Route
-              path="/home/signin"
-              element={<SignIn />}
-            />
-            <Route
-              path="/home/forgotpass"
-              element={<ForgotPassword />}
-            />
-            <Route
-              path="/home/resetpass/:token"
-              element={<ResetPassword />}
-            /> */}
-            <Route element={<TokenCheck />}>
-              <Route
-                path="/profile"
-                element={<Profile />}
-              />
-              <Route
-                path="/profile/editprofile"
-                element={<ProfileEdit />}
-              />
-              <Route
-                path="/profile/newrelease"
-                element={<NewRelease />}
-              />
-              <Route
-                path="/profile/checkoutpage"
-                element={<CheckoutPage />}
-              />
-              <Route
-                path="/profile/singles"
-                element={<Singles />}
-              />
-              <Route
-                path="/profile/singleedit/:id"
-                element={<SingleEdit />}
-              />
-              <Route
-                path="/profile/singleview/:id"
-                element={<SingleView />}
-              />
-            </Route>
-          </Routes>
-        </section>
-      {/* </Router> */}
+        <Outlet />
+      </section>
       <ToastContainer
         autoClose={3000}
         limit={1}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import { store } from "./app/store";
 import MaximizerApp from "./MaximizerApp";
 import Home from "./pages/Home";
@@ -12,50 +17,169 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
+const Profile = lazy(() => import("./pages/Profile"));
+import ProfileEdit from "./pages/ProfileEdit";
+import Singles from "./pages/Singles";
+import SingleEdit from "./pages/SingleEdit";
+import SingleView from "./pages/SingleView";
+import NewRelease from "./pages/NewRelease";
+const CheckoutPage = lazy(() => import("./pages/CheckoutPage"));
+import TokenCheck from "./components/TokenCheck";
 import Spinner from "./components/Spinner";
 import "./css/style.module.css";
 // TODO: Add ErrorBoundary
-// Migrate to RouterProvider
+// Create redirect for incorrect urls
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
+
+const elementRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<MaximizerApp />}
+      errorElement={<div>Error</div>}
+    >
+      <Route
+        path="/home"
+        element={<Home />}
+      />
+      <Route
+        path="/home/aboutus"
+        element={<AboutUs />}
+      />
+      <Route
+        path="/home/faq"
+        element={<FAQ />}
+      />
+      <Route
+        path="/home/signup/:token"
+        element={<SignUp />}
+      />
+      <Route
+        path="/home/emailsignup"
+        element={<EmailSignUp />}
+      />
+      <Route
+        path="/home/signin"
+        element={<SignIn />}
+      />
+      <Route
+        path="/home/forgotpassword"
+        element={<ForgotPassword />}
+      />
+      <Route
+        path="/home/resetpassword/:token"
+        element={<ResetPassword />}
+      />
+      <Route element={<TokenCheck />}>
+        <Route
+          path="/profile"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <Profile />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/profile/editprofile"
+          element={<ProfileEdit />}
+        />
+        <Route
+          path="/profile/singles"
+          element={<Singles />}
+        />
+        <Route
+          path="/profile/singleedit/:id"
+          element={<SingleEdit />}
+        />
+        <Route
+          path="/profile/singleview/:id"
+          element={<SingleView />}
+        />
+        <Route
+          path="/profile/newrelease"
+          element={<NewRelease />}
+        />
+        <Route
+          path="/profile/checkoutpage"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <CheckoutPage />
+            </Suspense>
+          }
+        />
+      </Route>
+    </Route>
+  )
+);
+
 const router = createBrowserRouter([
   {
-    path: "*",
+    path: "/",
     element: <MaximizerApp />,
     errorElement: <div>Error</div>,
     children: [
       {
         path: "home",
-        element: <Home />
+        element: <Home />,
       },
       {
         path: "home/aboutus",
-        element: <AboutUs />
+        element: <AboutUs />,
       },
       {
         path: "home/faq",
-        element: <FAQ />
+        element: <FAQ />,
       },
       {
         path: "home/signup/:token",
-        element: <SignUp />
+        element: <SignUp />,
       },
       {
         path: "home/emailsignup",
-        element: <EmailSignUp />
+        element: <EmailSignUp />,
       },
       {
         path: "home/signin",
-        element: <SignIn />
+        element: <SignIn />,
       },
       {
         path: "home/forgotpassword",
-        element: <ForgotPassword />
+        element: <ForgotPassword />,
       },
       {
         path: "home/resetpassword/:token",
-        element: <ResetPassword />
+        element: <ResetPassword />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+        loader: () => <TokenCheck />,
+      },
+      {
+        path: "/profile/editprofile",
+        element: <ProfileEdit />,
+      },
+      {
+        path: "/profile/singles",
+        element: <Singles />,
+      },
+      {
+        path: "/profile/singleedit/:id",
+        element: <SingleEdit />,
+      },
+      {
+        path: "/profile/singleview/:id",
+        element: <SingleView />,
+      },
+      {
+        path: "/profile/checkoutpage",
+        element: <CheckoutPage />,
+      },
+      {
+        path: "/profile/newrelease",
+        element: <NewRelease />,
       },
     ],
   },
@@ -64,7 +188,10 @@ const router = createBrowserRouter([
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} fallbackElement={<Spinner />} />
+      <RouterProvider
+        router={elementRouter}
+        fallbackElement={<Spinner />}
+      />
     </Provider>
   </React.StrictMode>
 );
