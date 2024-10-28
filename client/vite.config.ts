4c8261ed4ea5b452cc62e411fc5fa69bd6fdc8f6
andrewg117 <andrewjgrant7@gmail.com>
2024-10-28 17:31:47 -0400

    Added postcss-nesting for Chrome

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
+import postcssNesting from "postcss-nesting";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+        plugins: [
+            postcssNesting
+        ],
+    },
+},
   server: {
     open: "http://localhost:3000/",
     port: 3000,
